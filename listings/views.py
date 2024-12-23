23708c5c7fc349325911e194a6bd0f4c8ec3278a
andy cheung <andycheung.cs3@gmail.com>
2024-12-23 17:33:57 +0800

    step7 add listings template

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from .models import Listing
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
-def listing(request):
+    #listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings' : paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 def search(request):
     return render(request, 'listings/search.html')
